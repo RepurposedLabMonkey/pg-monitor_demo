0d9c79dfd653a527467af03d216db10c28e224aa
RepurposedLabMonkey <RepurposedLabMonkey@users.noreply.github.com>
2018-10-22 15:23:24 -0700

    added dotenv, nodemon not working

---
+++ JavaScript/db/index.js
@@ -0,0 +1,50 @@
+'use strict';
+
+// Bluebird is the best promise library available today,
+// and is the one recommended here:
+const promise = require('bluebird');
+const repos = require('./repos'); // loading all repositories
+
+//add .env config file to hide password on git repo
+// .env is listed in .gitignore
+const dotenv = require('dotenv'); //get dotenv
+dotenv.config() //load .env into process.env
+
+// pg-promise initialization options:
+const initOptions = {
+    // Use a custom promise library, instead of the default ES6 Promise:
+    promiseLib: promise,
+    // Extending the database protocol with our custom repositories;
+    // API: http://vitaly-t.github.io/pg-promise/global.html#event:extend
+    extend(obj, dc) {
+        // Database Context (dc) is mainly useful when extending multiple databases
+        // with different access API-s.
+        // Do not use 'require()' here, because this event occurs for every task
+        // and transaction being executed, which should be as fast as possible.
+        obj.users = new repos.Users(obj, pgp);
+        obj.products = new repos.Products(obj, pgp);
+    }
+};
+
+// Database connection parameters:
+const config = {
+    host: process.env.HOST,
+    port: process.env.PORT,
+    database: process.env.DATABASE,
+    password: process.env.PASSWORD,
+    user: process.env.USER
+};
+
+// Load and initialize pg-promise:
+const pgp = require('pg-promise')(initOptions);
+
+// Create the database instance:
+const db = pgp(config);
+
+// Load and initialize optional diagnostics:
+const diagnostics = require('./diagnostics');
+diagnostics.init(initOptions);
+
+// If you ever need access to the library's root (pgp object), you can do it via db.$config.pgp
+// See: http://vitaly-t.github.io/pg-promise/Database.html#.$config
+module.exports = db;
